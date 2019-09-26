712a974b57b0440fc7b67ba474d2490b5b2638ca
unknown <IFSC@CDR-750879.cacador.ifsc.edu.br>
2019-09-25 22:40:57 -0300

    Concluído teste unitário usuário

--- models/Usuario.php
+++ models/Usuario.php
@@ -10,26 +10,26 @@
 
         public function logar($login, $senha){
             $conexaoDB = $this->conectarBanco();// cpnecta com o banco.
-            $sql = $conexaoDB->prepare("select login, nome, email, celular from usuario 
+            $sql = $conexaoDB->prepare("select nome, login, email, celular from usuario 
                                         where
                                         login = ?
                                         and
-                                        senha = ?");// proteje contra sql inject.
+                                        senha = ?");// proteje contra sql injection.
             $sql->bind_param("ss", $login, $senha);
             $sql->execute();// executa o banco.
-
+            
             $resultado = $sql->get_result(); // recebe o resultado do banco de dados.
 
             if($resultado->num_rows === 0){ // se o resultado for igual a 0 os atributos serão null.
-                $this->login = null;
                 $this->nome = null;
+                $this->login = null;
                 $this->email = null;
                 $this->celular = null;
                 $this->logado = FALSE;
             }else{
-                while($linha = $resultado->fatch_assoc()){ // senão as variáveis receberão um array com as informações.
-                    $this->login = $linha['login'];
+                while($linha = $resultado->fetch_assoc()){ // senão as variáveis receberão um array com as informações.
                     $this->nome = $linha['nome'];
+                    $this->login = $linha['login'];
                     $this->email = $linha['email'];
                     $this->celular = $linha['celular'];
                     $this->logado = TRUE; 
@@ -40,21 +40,24 @@
             return $this->logado; // retorna se está logado ou não.
         }
 
-        public function incluirUsuario($nome, $email, $login, $senha){
-            $conexaoDB = $this->conectarBanco();
+        public function incluirUsuario($nome, $login, $senha, $email, $celular){
+            $conexaoDB = $this->conectarBanco(); // recebe um objeto de conexão.
 
-            $sqlInsert = $conexaoDB->prepare("insert into usuario
-                                            (nome, email, login, senha)
+            $sqlInsert = $conexaoDB->prepare("insert into usuario  
+                                            (nome, login, senha, email, celular)
                                             values
-                                            (?, ?, ?, ?)");
-            $sqlInsert->bind_param("ssss", $nome, $email, $login, $senha);
+                                            (?, ?, ?, ?, ?)");  // recebe os dados sem abrir brecha para SQLInjection.
+            $sqlInsert->bind_param("sssss", $nome, $login, $senha, $email, $celular);
 
-            $sqlInsert->execute();
-            
-            return TRUE;
+            $sqlInsert->execute();  // executa o sqlInsert.
+            if(!$sqlInsert->error){  // verifica se dá algum erro no sqlInsert.
+                return TRUE;
+            }else{
+                return FALSE;
+            }
         }
 
-        private function conectarBanco(){
+        private function conectarBanco(){ // cria um objeto (que é uma conexão com o Banco).
             $conn = new \mysqli('localhost', 'root', '', 'prospects');
             return $conn;
         }
--- tests/UsuarioTest.php
+++ tests/UsuarioTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace tests;
 
-require_once('../vendor/autoload.php');
+require_once('../vendor/autoload.php'); // recebe requerimentos da biblioteca PHPUnit.
 require_once('../models/Usuario.php');
 
 use models\Usuario;
@@ -9,21 +9,23 @@ use PHPUnit\Framework\TestCase;
 
 class UsuarioTest extends TestCase{
     /** @test */
-    public function testLogar(){
-        $usuario = new Usuario();
+    public function testLogar(){  // função para testar o login.
+        $usuario = new Usuario(); // cria um novo usuario.
         
-        $this->assertEquals(
-            TRUE,
-            $usuario->logar('usuario', 'senha')
+        $this->assertEquals(  // função que recebe dois parâmetros. 
+            TRUE,  // 1º O que eu espero que retorne.
+            $usuario->logar('lucas', '123') // 2º Parâmetro logar.
         );
-    }
 
+        unset($usuario);  // função para não ficar alocado na memória.
+    }
+    /** @test */
     public function testIncluirUsuario(){
         $usuario = new Usuario();
         
         $this->assertEquals(
             TRUE,
-            $usuario->incluirUsuario('Lucas', 'lmattos@mail.com', 'lmattos', '123456')
+            $usuario->incluirUsuario('Lucas', 'lmattos', '', 'lmattos@mail.com', '123456')
         );
         unset($usuario);
     }
