da1bf5f6200d0238f3cde7aecad1df2eda0e359e
lmattos1603 <lmattos1603@gmail.com>
2019-09-29 21:52:56 -0300

    Concluído testes na classe prospect

--- models/Prospect.php
+++ models/Prospect.php
@@ -10,120 +10,115 @@ class Prospect{
         public $whatsapp;
         public $rua;
         public $numero;
+        public $facebook;
         public $bairro;
         public $cidade;
         public $estado;
         public $cep;
         public $encontrado;
 
-        public function inserirProspect($nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $bairro, $cidade, $estado, $cep){
+        public function inserirProspect($nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $facebook, $bairro, $cidade, $estado, $cep){
             $conexaoDB = $this->conectarBanco(); // recebe um objeto de conexão.
 
-            $sqlInsert = $conexaoDB->prepare("insert into prospect  
-                                            (nome, cpf, email, telefone, whatsapp, rua, numero, bairro, cidade, estado, cep)
+            $sqlInsert = $conexaoDB->prepare("insert into prospecto  
+                                            (nome, cpf, email, telefone, whatsapp, rua, numero, facebook, bairro, cidade, estado, cep)
                                             values
-                                            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");  // recebe os dados sem abrir brecha para SQLInjection.
-            $sqlInsert->bind_param("sssssssssss", $nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $bairro, $cidade, $estado, $cep);
+                                            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");  // recebe os dados sem abrir brecha para SQLInjection.
+            $sqlInsert->bind_param("ssssssssssss", $nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $facebook, $bairro, $cidade, $estado, $cep);
 
             $sqlInsert->execute();  // executa o sqlInsert.
             if(!$sqlInsert->error){  // verifica se dá algum erro no sqlInsert.
-                return TRUE;
+                $retorno = TRUE;
             }else{
-                return FALSE;
+                $retorno = FALSE;
             }
+            $sqlInsert->close(); // fecha o sql.
+            $conexaoDB->close(); // fecha a conexão.
+            return $retorno;
         }
 
-        public function editarProspect($nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $bairro, $cidade, $estado, $cep, $id){
+        public function editarProspect($nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $facebook, $bairro, $cidade, $estado, $cep, $id){
             $conexaoDB = $this->conectarBanco(); // recebe um objeto de conexão.
 
             $sqlUpdate = $conexaoDB->prepare("update prospect set 
                                             nome = ?, cpf = ?, email = ?, telefone = ?, whatsapp = ?, rua = ?,
-                                            numero = ?, bairro = ?, cidade = ?, estado = ?, cep = ?
+                                            numero = ?, facebook = ?, bairro = ?, cidade = ?, estado = ?, cep = ?
                                             where
                                             id = ?");  // recebe os dados sem abrir brecha para SQLInjection.
-            $sqlUpdate->bind_param("sssssssssssi", $nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $bairro, $cidade, $estado, $cep, $id);
+            $sqlUpdate->bind_param("ssssssssssssi", $nome, $cpf, $email, $telefone, $whatsapp, $rua, $numero, $facebook, $bairro, $cidade, $estado, $cep, $id);
 
             $sqlUpdate->execute();  // executa o sqlUpdate.
             if(!$sqlUpdate->error){  // verifica se dá algum erro no sqlInsert.
-                return TRUE;
+                $retorno = TRUE;
             }else{
-                return FALSE;
+                $retorno = FALSE;
             }
+            $sqlInsert->close(); // fecha o sql.
+            $conexaoDB->close(); // fecha a conexão.
+            return $retorno;
         }
 
-        public function buscarProspect($cpf){
-            $conexaoDB = $this->conectarBanco();// cpnecta com o banco.
-            $sql = $conexaoDB->prepare("select cpf from prospect 
-                                        where
-                                        cpf = ?");// proteje contra sql injection.
-            $sql->bind_param("s", $cpf);
-            $sql->execute();// executa o banco.
-            
-            $resultado = $sql->get_result(); // recebe o resultado do banco de dados.
+        public function buscarProspect($email=null){
+            $conexaoDB = $this->conectarBanco();// conecta com o banco.
+            $prospects = array();
 
-            if($resultado->num_rows === 0){ // se o resultado for igual a 0 os atributos serão null.
-                $this->nome = NULL;
-                $this->cpf = NULL;
-                $this->email = NULL;
-                $this->telefone = NULL;
-                $this->whatsapp = NULL;
-                $this->rua = NULL;
-                $this->numero = NULL;
-                $this->bairro = NULL;
-                $this->cidade = NULL;
-                $this->estado = NULL;
-                $this->cep = NULL;
-                $this->encontrado = FALSE;
+            if($email === null){
+                $sqlBusca = $conexaoDB->prepare("select id, nome, cpf, email, telefone, whatsapp, rua, numero, facebook, bairro, cidade, estado, cep 
+                                            from prospect");// proteje contra sql injection.
+                $sqlBusca->execute();// executa o banco.
             }else{
-                while($linha = $resultado->fetch_assoc()){ // senão as variáveis receberão um array com as informações.
-                    $this->nome = $linha['nome'];
-                    $this->cpf = $linha['cpf'];
-                    $this->email = $linha['email'];
-                    $this->telefone = $linha['telefone'];
-                    $this->whatsapp = $linha['whatsapp'];
-                    $this->rua = $linha['rua'];
-                    $this->numero = $linha['numero'];
-                    $this->bairro = $linha['bairro'];
-                    $this->cidade = $linha['cidade'];
-                    $this->estado = $linha['estado'];
-                    $this->cep = $linha['cep'];
-                    $this->encontrado = TRUE; 
+                $sqlBusca = $conexaoDB->prepare("select id, nome, cpf, email, telefone, whatsapp, rua, numero, facebook, bairro, cidade, estado, cep 
+                                        from prospect 
+                                        where
+                                        email = ?");// proteje contra sql injection.
+                $sqlBusca->bind_param("s", $email);
+                $sqlBusca->execute();// executa o banco.
+            }
+            
+            $resultado = $sqlBusca->get_result(); // recebe o resultado do banco de dados.
+
+            if($resultado->num_rows !== 0){ // se o resultado for diferente de 0 os atributos não serão null.
+                while($linha = $resultado->fetch_assoc()){ // as variáveis receberão um array com as informações.
+                    $prospect = array(
+                        "id" => $linha['nome'],
+                        "cpf" => $linha['cpf'],
+                        "email" => $linha['email'],
+                        "telefone" => $linha['telefone'],
+                        "whatsapp" => $linha['whatsapp'],
+                        "rua" => $linha['rua'],
+                        "numero" => $linha['numero'],
+                        "facebook" => $linha['facebook'],
+                        "bairro" => $linha['bairro'],
+                        "cidade" => $linha['cidade'],
+                        "estado" => $linha['estado'],
+                        "cep" => $linha['cep']
+                    );
+                    $prospects[] = $prospect; 
                 }
             }
-            $sql->close(); // fecha o sql.
+
+            $sqlBusca->close(); // fecha o sql.
             $conexaoDB->close(); // fecha a conexão.
-            return $this->encontrado; // retorna se está logado ou não.
+            return $prospects; // retorna se está logado ou não.
         }
 
-        public function deletarProspect($cpf){
+        public function deletarProspect($id){
             $conexaoDB = $this->conectarBanco(); // recebe um objeto de conexão.
 
-            $sqlSelect = $conexaoDB->prepare("select cpf from prospect
+            $sqlDelete = $conexaoDB->prepare("delete from prospecto
                                             where 
-                                            cpf = ?");  // recebe os dados sem abrir brecha para SQLInjection.
-            $sqlSelect->bind_param("s", $cpf);
-
-            $sqlSelect->execute();  // executa o sqlUpdate.
-
-            $resultado = $sql->get_result(); // recebe o resultado do banco de dados.
-
-            if($resultado->num_rows === 0){ // se o resultado for igual a 0 os atributos serão null.
-                $this->encontrado = FALSE;
-            }else{
-                $this->encontrado = TRUE;
-                $sqlDelete = $conexaoDB->prepare("delete from prospect
-                                            where 
-                                            cpf = ?");  // recebe os dados sem abrir brecha para SQLInjection.
-                $sqlDelete->bind_param("s", $cpf);
-
-                $sqlDelete->execute();
-            }
+                                            id = ?");  // recebe os dados sem abrir brecha para SQLInjection.
+            $sqlDelete->bind_param("i", $id);
+            $sqlDelete->execute();  // executa o sqlUpdate.
 
             if(!$sqlDelete->error){  // verifica se dá algum erro no sqlDelete.
-                return TRUE;
+                $retorno = TRUE;
             }else{
-                return FALSE;
+                $retorno = FALSE;
             }
+            $sqlDelete->close(); // fecha o sql.
+            $conexaoDB->close(); // fecha a conexão.
+            return $retorno;
         }
 
         private function conectarBanco(){ // cria um objeto (que é uma conexão com o Banco).
